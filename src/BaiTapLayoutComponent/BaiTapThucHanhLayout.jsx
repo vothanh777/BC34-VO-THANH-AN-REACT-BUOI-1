1dec1cea995817e70db4d99ce7f2d8c2ff387b35
vothanhan777 <vothanhan777@gmail.com>
2022-09-15 23:04:35 +0700

    complete homework react buoi 01

---
+++ src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Body from "./Body";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default class BaiTapThucHanhLayout extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    );
+  }
+}
---
+++ src/BaiTapLayoutComponent/Body.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Banner from "./Banner";
+import Item from "./Item";
+
+export default class Body extends Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+        <Item />
+      </div>
+    );
+  }
+}
